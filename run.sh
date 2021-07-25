d7d975da349ead11208ba82ccdde9b62be359e9f
Stanislav <stasyatr@gmail.com>
2021-07-25 22:47:53 +0300

    (Added) openvpn server creation

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -v --vault-password-file vault_password_file -i ./inventories/inventory.yml playbook.yml
+rm -rf certificates && ansible-playbook -v --vault-password-file vault_password_file -i ./inventories/inventory.yml playbook.yml
